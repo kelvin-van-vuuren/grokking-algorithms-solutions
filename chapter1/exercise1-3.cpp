5eeade91b41a02d39f19ff96f79f76f7660e9ae7
Kelvin van Vuuren <kelvin@kelvinvv.xyz>
2023-05-18 23:19:06 +0100

    Exercise 1-3: search name in phone book

---
+++ chapter1/exercise1-3.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+/* You have a name, and you want to find the person's phone number in the phone book. */
+
+int main()
+{
+    std::cout << "A phone book is sorted alphabetically. Therefore, you could use a binary search to find the person's "
+                 "name in O(logn) time."
+              << std::endl;
+}
