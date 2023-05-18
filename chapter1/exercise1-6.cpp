c8f095310e2e5adbc0c084d53b26c1e23283e624
Kelvin van Vuuren <kelvin@kelvinvv.xyz>
2023-05-18 23:25:15 +0100

    Exercise 1-6: read just As from phone book

---
+++ chapter1/exercise1-6.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+/* You want to read the numbers of just the As. */
+
+int main()
+{
+    std::cout << "This will still take O(n) despite only needing to search 1/26th of the phone book." << std::endl;
+}
