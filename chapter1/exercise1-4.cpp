9b04281fb23096fd2f2e0b315a44627f6ebe58d5
Kelvin van Vuuren <kelvin@kelvinvv.xyz>
2023-05-18 23:21:20 +0100

    Exercise 1-4: search by number in phone book

---
+++ chapter1/exercise1-4.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+/* You have a phone number, and  you want to find the person's name in the phone book. */
+
+int main()
+{
+    std::cout << "You will need to perform a linear search, which will take O(n) times where n is the number of phone "
+                 "numbers."
+              << std::endl;
+}
