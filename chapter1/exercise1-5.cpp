afdfa0a4b1796ba872afd0ddf27b3c44a820e251
Kelvin van Vuuren <kelvin@kelvinvv.xyz>
2023-05-18 23:23:23 +0100

    Exercise 1-5: read each number in phone book

---
+++ chapter1/exercise1-5.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+/* You want to read the numbers of every person in the phone book */
+
+int main()
+{
+    std::cout << "This requires going through each person in the phone book, which will take O(n) time." << std::endl;
+}
