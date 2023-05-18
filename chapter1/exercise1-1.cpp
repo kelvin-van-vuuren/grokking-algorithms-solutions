9559efbf86f6c80f12b4b4d3b5f4ba06ce3250cb
Kelvin van Vuuren <kelvin@kelvinvv.xyz>
2023-05-18 15:04:03 +0100

    Exercise 1-1: binary search 128 items worst case

---
+++ chapter1/exercise1-1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+/* Suppose you have a sorted list of 128 names, and you're searching through it using binary search. What's the maximum
+ * number of steps it would take? */
+
+int main()
+{
+    std::cout << "Binary search has time complexity O(logn), therefore for 128 names the worst case would take log128 = 7 steps" << std::endl;
+}
